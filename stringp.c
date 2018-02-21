8ca5a0084a83ee7da6efc546e291cc1db38e4697
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-02-21 15:42:14 +0530

    Update stringp.c

--- stringp.c
+++ stringp.c
@@ -1,24 +1,16 @@
 #include<stdio.h>
-int main()
-{
+int main(){
 int rev=0,t;
 char a;
 printf("enter the string:");
 scanf("%s",&a);
 t=a;
-while(t!='\0')
-{
+while(t!='\0'){
 rev=rev*10;
 rev=rev+t%10;
-t=t/10;
-}
-if(a==rev)
-{
-printf("it is a palindrome");
-}
-else
-{
-printf("it is not a palindrome");
-}
-return 0;
-}
+t=t/10;}
+if(a==rev){
+printf("it is a palindrome");}
+else{
+printf("it is not a palindrome");}
+return 0;}
