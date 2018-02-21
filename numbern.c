f28e83b2d38e1e89ee75d77970f05e0a8d530d7d
rockermoney <35295528+rockermoney@users.noreply.github.com>
2018-02-21 15:49:19 +0530

    Create numbern.c

---
+++ numbern.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{int a,b,c;
+printf("enter the number:");
+scanf("%d",&a);
+printf("left and right numbers:");
+scanf("%d%d",&b,&c);
+if(a>b&&a<c)
+{printf("yes");
+}else
+{printf("no");}
+return 0;}
